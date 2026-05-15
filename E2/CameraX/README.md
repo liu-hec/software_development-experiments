af49622a40d29c038090a65c1d57568f206ceced
liubingcheng <121052023019@student.fjnu.edu.cn>
2026-05-15 19:35:31 +0800

    实验2-5

---
+++ E2/CameraX/README.md
@@ -0,0 +1,174 @@
+
+
+# CameraDemo
+
+## 一 目的
+
+   完成一个基于cameraX的相机app
+
+
+
+## 二 实现过程
+
+​     明确gradle和cameraX的依赖适配性，采用传统的xml形式完成ui设计，注意兼容性。以gradle9.4.1为例
+
+  1**.前提准备**
+
+
+
+  **开启视图绑定**
+
+​    **ViewBinding（视图绑定）** 是 Android 专门给 **XML 布局** 提供的工具，作用是 **替代繁琐的 `findViewById`**，安全、简洁地获取界面控件（按钮、文本、布局等）。同时创建activity_main.xml主布局页面。
+
+
+
+   **配置权限**
+
+```
+<uses-feature android:name="android.hardware.camera.any" />  <!--前置或者后置-->
+<uses-permission android:name="android.permission.CAMERA" />
+<uses-permission android:name="android.permission.RECORD_AUDIO"/>
+<uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"
+    android:maxSdkVersion="28" />
+```
+
+
+
+
+
+2. **基本布局**
+
+​    在activity_main中创建两个button，完成ui设计。
+
+​    
+
+​       创建基本的页面图，和基本的代码结构。绑定界面布局，检查权限和处理button点击事件，启动异步线程（为analysis的处理线程）
+
+```
+companion object {
+    private const val TAG = "CameraXApp"
+    private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+    private const val REQUEST_CODE_PERMISSIONS = 10
+    private val REQUIRED_PERMISSIONS =
+        mutableListOf (
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO
+        ).apply {
+            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            }
+        }.toTypedArray()
+} //存放固定不变的配置：权限列表、照片名字格式、日志标记
+```
+
+
+
+3.  **完成权限请求方法**
+
+![image-20260515174015712](C:/Users/86152/AppData/Roaming/Typora/typora-user-images/image-20260515174015712.png)
+
+
+
+4.**核心流程**
+
+
+
+   完善startCamera方法，这是核心方法，融合其他的具体实现例如takephoto方法。
+
+  获取 CameraProvider（相机总管）：`ProcessCameraProvider.getInstance(this)`
+
+   等相机初始化完成后：
+
+- ​        创建 **Preview** 用例
+- ​        创建 **ImageCapture** 用例（拍照）
+- ​        创建 **VideoCapture** 用例（录像）
+- ​        创建 **ImageAnalysis** 用例（分析亮度）
+
+​      核心流程：获取相机管理器 → 配置预览 → 选择摄像头 → 绑定 → 相机开始工作
+
+
+
+**Preview方法**
+
+```
+val preview = Preview.Builder().build()
+it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider) //将预览页面放在屏幕
+...
+..
+cameraProvider.unbindAll()
+cameraProvider.bindToLifecycle(...) //先清掉旧相机，再把所有功能绑定到生命周期上
+```
+
+
+
+**重写takephoto方法**
+
+​    流程：
+
+1. 确认 `imageCapture` 已准备好
+2. 生成文件名，按时间戳命名
+3. 选择保存目录（App 私有目录 `getExternalFilesDir("CameraX")`）
+4. 构建输出文件配置：`ImageCapture.OutputFileOptions.Builder(photoFile).build()`
+5. 拍照回调，拍成功弹出 Toast 提示，同时打印日志
+
+
+
+ **photo analysis方法**
+
+```
+val imageAnalyzer = ImageAnalysis.Builder().build().also {
+    it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
+        Log.d(TAG, "Average luminosity: $luma")
+    })
+} //每一帧画面都拿去算亮度，然后打印日志
+```
+
+![image-20260515185121064](C:/Users/86152/AppData/Roaming/Typora/typora-user-images/image-20260515185121064.png)
+
+
+
+**video方法**
+
+```
+recording = videoCapture.output.prepareRecording(...)
+    .start(...) //录制
+```
+
+![image-20260515185955556](C:/Users/86152/AppData/Roaming/Typora/typora-user-images/image-20260515185955556.png)
+
+ 
+
+  存放在MediaStore中，可以发现vedio录制成功
+
+![image-20260515190031059](C:/Users/86152/AppData/Roaming/Typora/typora-user-images/image-20260515190031059.png)
+
+
+
+
+
+```
+cameraProvider
+    .bindToLifecycle(this, cameraSelector,
+        preview,
+        imageCapture,
+        videoCapture,
+        imageAnalyzer
+    )  一次绑定所有的use cases
+```
+
+​            这样我们可以边录制，边拍照和分析。
+
+
+
+   这样完成了preview，photo,vedio和普通的analysis的混合支持。将相机的绑定到Activity的生命周期上。这样我们无需重写二外的生命周期函数，附属于Activity。
+
+
+
+## 三 总结
+
+​        通过本实验，能够基于 CameraX流程和AI辅助 完成一个功能完整的相机 App，实现多功能混合使用（预览、拍照、录像、图像分析），并了解相机生命周期管理的绑定、权限控制和文件保存的核心技能。
+
+
+
+
+
---
+++ E2/CameraX/app/src/main/java/com/example/camerademo/MainActivity.kt
@@ -0,0 +1,319 @@
+package com.example.camerademo
+import android.Manifest
+import android.content.ContentValues
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import androidx.camera.core.ImageCapture
+import androidx.camera.video.Recorder
+import androidx.camera.video.Recording
+import androidx.camera.video.VideoCapture
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.video.MediaStoreOutputOptions
+import androidx.camera.video.Quality
+import androidx.camera.video.QualitySelector
+import androidx.camera.video.VideoRecordEvent
+import androidx.core.content.PermissionChecker
+import com.example.camerademo.databinding.ActivityMainBinding
+import java.io.File
+import java.nio.ByteBuffer
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+typealias LumaListener = (luma: Double) -> Unit
+
+class CameraDemo : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityMainBinding
+
+    private var imageCapture: ImageCapture? = null
+
+    private var videoCapture: VideoCapture<Recorder>? = null
+    private var recording: Recording? = null
+
+    private lateinit var cameraExecutor: ExecutorService
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+        // Set up the listeners for take photo and video capture buttons
+        viewBinding.imageCaptureButton.setOnClickListener { takePhoto() }
+        viewBinding.videoCaptureButton.setOnClickListener { captureVideo() }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    private fun takePhoto() {
+        val imageCapture = imageCapture ?: return
+
+        // ① 创建文件名
+        val name = SimpleDateFormat(
+            FILENAME_FORMAT,
+            Locale.US
+        ).format(System.currentTimeMillis())
+
+        // ② 指定保存目录 ⭐⭐⭐
+        val photoDir = getExternalFilesDir("CameraX")
+
+        // 如果文件夹不存在就创建
+        if (photoDir != null && !photoDir.exists()) {
+            photoDir.mkdirs()
+        }
+
+        // ③ 创建最终文件
+        val photoFile = File(
+            photoDir,
+            "$name.jpg"
+        )
+
+        // ④ 创建输出配置（改为 File）
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        val executor = ContextCompat.getMainExecutor(this)
+
+        // ⑤ 拍照
+        imageCapture.takePicture(
+            outputOptions,
+            executor,
+            object : ImageCapture.OnImageSavedCallback {
+
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val msg = "Saved to: ${photoFile.absolutePath}"
+                    Toast.makeText(this@CameraDemo, msg, Toast.LENGTH_LONG).show()
+                    Log.d(TAG, msg)
+                }
+            }
+        )
+    }
+
+
+
+        private fun startCamera() {
+
+            val cameraProvider = ProcessCameraProvider.getInstance(this)
+            //获取相机管理器
+            cameraProvider.addListener({
+                // Used to bind the lifecycle of cameras to the lifecycle owner
+                val cameraProvider: ProcessCameraProvider = cameraProvider.get()
+                //获取provider
+                // Preview
+                val preview = Preview.Builder()
+                    .build()
+                    .also {
+                        it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
+                    }
+
+                imageCapture = ImageCapture.Builder().build()
+
+                val recorder = Recorder.Builder()
+                    .setQualitySelector(QualitySelector.from(Quality.HIGHEST))
+                    .build()
+                videoCapture = VideoCapture.withOutput(recorder)
+
+                //capture后 analyze
+                val imageAnalyzer = ImageAnalysis.Builder()
+                    .build()
+                    .also {
+                        it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
+                            Log.d(TAG, "Average luminosity: $luma")
+                        })
+                    }
+
+                // Select back camera as a default
+                val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA //默认后置
+
+                try {
+                    // Unbind use cases before rebinding
+                    cameraProvider.unbindAll()
+
+                    // Bind use cases to camera
+                    cameraProvider
+                        .bindToLifecycle(this, cameraSelector,
+                            preview,
+                            imageCapture,
+                            videoCapture,
+                            imageAnalyzer
+                        )
+                } catch(exc: Exception) {
+                    Log.e(TAG, "Use case binding failed", exc)
+                }
+
+            }, ContextCompat.getMainExecutor(this))
+        }
+
+
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    companion object {
+        private const val TAG = "CameraXApp"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf (
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
+    }
+
+
+      //权限请求
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) { //请求代码
+            if (allPermissionsGranted()) {  //已经授权
+                startCamera()
+            } else {
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+
+  //内部类实现analysis
+
+    private class LuminosityAnalyzer(private val listener: LumaListener) : ImageAnalysis.Analyzer {
+
+        private fun ByteBuffer.toByteArray(): ByteArray {
+            rewind()    // Rewind the buffer to zero
+            val data = ByteArray(remaining())
+            get(data)   // Copy the buffer into a byte array
+            return data // Return the byte array
+        }
+
+        override fun analyze(image: ImageProxy) {
+
+            val buffer = image.planes[0].buffer
+            val data = buffer.toByteArray()
+            val pixels = data.map { it.toInt() and 0xFF }
+            val luma = pixels.average()
+
+            listener(luma)
+
+            image.close()
+        }
+
+    }
+
+
+
+
+
+
+    //vedio
+    // Implements VideoCapture use case, including start and stop capturing.
+    private fun captureVideo() {
+        val videoCapture = this.videoCapture ?: return
+
+        viewBinding.videoCaptureButton.isEnabled = false
+
+        val curRecording = recording
+        if (curRecording != null) {
+            // Stop the current recording session.
+            curRecording.stop()
+            recording = null
+            return
+        }
+
+        // create and start a new recording session
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+            .format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "video/mp4")
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Video.Media.RELATIVE_PATH, "Movies/CameraX-Video")
+            }
+        }
+
+        val mediaStoreOutputOptions = MediaStoreOutputOptions //存放
+            .Builder(contentResolver, MediaStore.Video.Media.EXTERNAL_CONTENT_URI)
+            .setContentValues(contentValues)
+            .build()
+        recording = videoCapture.output
+            .prepareRecording(this, mediaStoreOutputOptions)
+            .apply {
+                if (PermissionChecker.checkSelfPermission(this@CameraDemo,
+                        Manifest.permission.RECORD_AUDIO) ==
+                    PermissionChecker.PERMISSION_GRANTED)
+                {
+                    withAudioEnabled()
+                }
+            }
+            .start(ContextCompat.getMainExecutor(this)) { recordEvent ->
+                when(recordEvent) {
+                    is VideoRecordEvent.Start -> {
+                        viewBinding.videoCaptureButton.apply {
+                            text = getString(R.string.stop_capture)
+                            isEnabled = true
+                        }
+                    }
+                    is VideoRecordEvent.Finalize -> {
+                        if (!recordEvent.hasError()) {
+                            val msg = "Video capture succeeded: " +
+                                    "${recordEvent.outputResults.outputUri}"
+                            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT)
+                                .show()
+                            Log.d(TAG, msg)
+                        } else {
+                            recording?.close()
+                            recording = null
+                            Log.e(TAG, "Video capture ends with error: " +
+                                    "${recordEvent.error}")
+                        }
+                        viewBinding.videoCaptureButton.apply {
+                            text = getString(R.string.start_capture)
+                            isEnabled = true
+                        }
+                    }
+                }
+            }
+    }
+
+
+
+
+}
